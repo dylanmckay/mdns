e14ae12d5dab3108cc947b61d62a02a416b953cc
Robin Appelman <robin@icewind.nl>
2020-01-27 21:46:53 +0100

    add methods for resolving known hosts instead of discovering all hosts

---
+++ examples/resolve_hosts.rs
@@ -0,0 +1,18 @@
+use mdns::Error;
+use std::time::Duration;
+
+const SERVICE_NAME: &'static str = "_http._tcp.local";
+const HOSTS: [&'static str; 2] = ["server1._http._tcp.local", "server2._http._tcp.local"];
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let responses = mdns::resolve::multiple(SERVICE_NAME, &HOSTS, Duration::from_secs(15)).await?;
+
+    for response in responses {
+        if let (Some(host), Some(ip)) = (response.hostname(), response.ip_addr()) {
+            println!("found host {} at {}", host, ip)
+        }
+    }
+
+    Ok(())
+}
